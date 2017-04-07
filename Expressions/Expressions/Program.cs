a00bb15776edb3025d6326a2a7f2b45cc6995a70
Ayman Alkorty <al.korty@hotmail.com>
2017-04-07 14:51:56 +0300

    e

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float myVar = 10.5F; // operant
+            float myVar2 = 10.7F;  //operant
+
+            float temp = myVar + myVar2; 
+            
+            //basic operators سهلة + - / * 
+
+            // صعبة bitwise operations
+            //logical operators boolen true , fuls
+
+            //1- unary operand
+            //2- binary operands
+            //3- 3 operands  ((condetionar operator المعامل الشرطي))
+
+
+
+
+
+
+        }
+    }
+}
