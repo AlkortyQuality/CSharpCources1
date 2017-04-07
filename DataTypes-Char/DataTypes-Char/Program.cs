d756c98ea7bb128ee37d01acafc8fed62b48443b
Ayman Alkorty <al.korty@hotmail.com>
2017-04-07 14:56:42 +0300

    cources

---
+++ DataTypes-Char/DataTypes-Char/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue;
+                int charMax = char.MaxValue;
+            char myVar = 'G';
+            int myVarValue = 'G';
+            char myvarvalue2 = Convert.ToChar(myVarValue);
+            Console.WriteLine("Min Char = {0} Max Cahr = {1} My Character = {2} my variable Number = {3} my conversion = {4} ", charMin, charMax, myVar, myVarValue , myvarvalue2);
+
+
+            string mystring = "this is my variable";
+            Console.WriteLine("\nmy string is  {0}", mystring);
+
+            bool myBool = true;
+            Console.WriteLine("\n my boolean value is {0}", myBool);
+
+            Console.ReadKey();
+        }
+    }
+}
