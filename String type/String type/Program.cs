6d8b8525cca0708f1e462c66465525dd6f41454e
Ayman Alkorty <al.korty@hotmail.com>
2017-04-07 14:19:05 +0300

    MY FIRS VARIABLE PROGRAM
    This is my firs programe i did wit vs

---
+++ String type/String type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //\'\"\\ \0 \a \b \n \r \t \v \f 
+
+
+
+            String mystring = "Ayman said This is a good\q course, and i think ";
+            Console.WriteLine("{0}", mystring);
+            Console.ReadKey();
+
+        }
+    }
+}
